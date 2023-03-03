0e3d8811bbd78d90de5b291f507e3583596850f4
James Kent <james.kent@austin.utexas.edu>
2023-03-03 14:38:17 -0600

    Delete .spectral.mjs

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjU0NTA4/spectral.js?branch_slug=main&token=623e7421-01bd-4f90-b9fe-61e27a0e73b0";
-export default { extends: ruleset };
